b30b506fbdd65a26edfc764b2855d0c9a352aede
Mikael Fiala-Lundstrom <millom@gmail.com>
2024-08-12 20:20:50 +0200

    CSS Grid: Fix layout

--- src/pages/LandingPage.tsx
+++ src/pages/LandingPage.tsx
@@ -2,9 +2,9 @@ import { ReactElement } from "react";
 
 export function LandingPage(): ReactElement {
   return (
-    <>
+    <div className="main-content">
       <h1>LandingPage</h1>
       <p>LandingPage 2</p>
-    </>
+    </div>
   );
 }
--- src/pages/SearchPage.tsx
+++ src/pages/SearchPage.tsx
@@ -2,9 +2,9 @@ import { ReactElement } from "react";
 
 export function SearchPage(): ReactElement {
   return (
-    <>
+    <div className="main-content">
       <h1>SearchPage</h1>
       <p>SearchPage 2</p>
-    </>
+    </div>
   );
 }
