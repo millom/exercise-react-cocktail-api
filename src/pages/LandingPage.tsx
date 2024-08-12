a8efb2e94fd1caa1b71353d1ba3dcfe7d91db034
Mikael Fiala-Lundstrom <millom@gmail.com>
2024-08-12 22:29:23 +0200

    LandingPage: Try to Fetch, fail with HTML instead of JSON

--- src/pages/LandingPage.tsx
+++ src/pages/LandingPage.tsx
@@ -1,10 +1,55 @@
-import { ReactElement } from "react";
+import { ReactElement, useEffect, useState } from "react";
+
+// const [cocktails, setCocktails] = useState([]);
 
 export function LandingPage(): ReactElement {
+  const getCocktails = () => {
+    fetch("www.thecocktaildb.com/api/json/v1/1/random.php")
+      // fetch("www.thecocktaildb.com/api/json/v1/1/random.php", {
+      //   headers: {
+      //     "Content-Type": "application/json",
+      //     Accept: "application/json",
+      //   },
+      // })
+      // fetch("www.thecocktaildb.com/api/json/v1/1/search.php?s=margarita")
+      // fetch("www.thecocktaildb.com/api/json/v1/1/search.php?f=m")
+      .then((response) => {
+        let data = response.json();
+        console.log(data);
+        return data;
+      })
+      .then((resData) => {
+        console.log(resData);
+        // setCocktails(resData);
+      });
+  };
+
+  // useEffect(() => {
+  //   fetch("www.thecocktaildb.com/api/json/v1/1/random.php")
+  //     // fetch("www.thecocktaildb.com/api/json/v1/1/random.php", {
+  //     //   headers: {
+  //     //     "Content-Type": "application/json",
+  //     //     Accept: "application/json",
+  //     //   },
+  //     // })
+  //     // fetch("www.thecocktaildb.com/api/json/v1/1/search.php?s=margarita")
+  //     // fetch("www.thecocktaildb.com/api/json/v1/1/search.php?f=m")
+  //     .then((response) => {
+  //       let data = response;
+  //       console.log(data);
+  //       return data.json();
+  //     })
+  //     .then((resData) => {
+  //       console.log(resData);
+  //       // setCocktails(resData);
+  //     });
+  // }, []);
+
   return (
     <div className="main-content">
       <h1>LandingPage</h1>
       <p>LandingPage 2</p>
+      <button onClick={getCocktails}>Click</button>
     </div>
   );
 }
