8eb89df42eeabddf2e57b26b5c27713b02228114
Mikael Fiala-Lundstrom <millom@gmail.com>
2024-08-13 16:28:36 +0200

    CocktailCard: Update css and layout

--- src/components/CocktailCard.tsx
+++ src/components/CocktailCard.tsx
@@ -9,9 +9,15 @@ interface ICocktailCardProps {
 export function CocktailCard({ cocktail }: ICocktailCardProps): ReactElement {
   // const { name, cocktail } = useCocktailsContext();
   return (
-    <div className="card">
+    <div className="card" onClick={() => {}}>
       {/* {name} */}
-      <p>Concktail id: {cocktail?.id}</p>
+      {/* <p>Concktail id: {cocktail?.id}</p> */}
+      <img
+        className="image"
+        src={cocktail?.imgSrc}
+        alt={"Cocktail:" + cocktail?.name}
+      />
+      <p className="card-text">{cocktail?.name}</p>
       {/* <img src="../assets/poster.png" alt="" /> */}
       {/* <h1>Header {name}</h1>
       <p>Header 2</p> */}
