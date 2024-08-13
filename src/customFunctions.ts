c1a0eabf042caa50dc0bc759c314e2153ca62a8c
Mikael Fiala-Lundstrom <millom@gmail.com>
2024-08-13 15:58:34 +0200

    IJSON, ICocktail: Fix interfaces

--- src/customFunctions.ts
+++ src/customFunctions.ts
@@ -1,11 +1,32 @@
-import { ICocktail } from "./interfaces";
+import { ICocktail, IJSON } from "./interfaces";
 
-export const jsonToCocktail: (drinks: ICocktail[]) => ICocktail[] = (
-  drinks: ICocktail[]
+export const jsonToCocktail: (drinks: IJSON[]) => ICocktail[] = (
+  drinks: IJSON[]
 ) => {
-  return drinks.map((json: ICocktail) => {
+  return drinks.map((json: IJSON) => {
     const cocktail: ICocktail = {
-      idDrink: json.idDrink,
+      id: json.idDrink,
+      name: json.strDrink,
+      category: json.strCategory,
+      isAlcoholic: json.strAlcoholic === "Alcoholic",
+      imgSrc: json.strDrinkThumb,
+      ingredients: [
+        json.strIngredient1,
+        json.strIngredient2,
+        json.strIngredient3,
+        json.strIngredient4,
+        json.strIngredient5,
+        json.strIngredient6,
+        json.strIngredient7,
+        json.strIngredient8,
+        json.strIngredient9,
+        json.strIngredient10,
+        json.strIngredient11,
+        json.strIngredient12,
+        json.strIngredient13,
+        json.strIngredient14,
+        json.strIngredient15,
+      ],
     };
     return cocktail;
   });
--- src/interfaces.ts
+++ src/interfaces.ts
@@ -1,10 +1,33 @@
-export interface ICocktail {
+export interface IJSON {
   idDrink: string;
+  strDrink: string;
+  strDrinkThumb: string;
+  strCategory: string;
+  strAlcoholic: string;
+  strIngredient1: string;
+  strIngredient2: string;
+  strIngredient3: string;
+  strIngredient4: string;
+  strIngredient5: string;
+  strIngredient6: string;
+  strIngredient7: string;
+  strIngredient8: string;
+  strIngredient9: string;
+  strIngredient10: string;
+  strIngredient11: string;
+  strIngredient12: string;
+  strIngredient13: string;
+  strIngredient14: string;
+  strIngredient15: string;
+}
+
+export interface ICocktail {
+  id: string;
   name: string;
   imgSrc: string;
   category: string;
   isAlcoholic: boolean;
-  ingredients: string[15];
+  ingredients: Array<string>;
 }
 
 export interface ICocktailContext {
