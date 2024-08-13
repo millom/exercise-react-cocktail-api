d2a7cd77fa6e06c52a12c86937008e4e1dfcc459
Mikael Fiala-Lundstrom <millom@gmail.com>
2024-08-13 13:39:19 +0200

    CocktailCard: Add id from global cocktail

--- src/components/CocktailCard.tsx
+++ src/components/CocktailCard.tsx
@@ -2,10 +2,11 @@ import { ReactElement } from "react";
 import { useCocktailsContext } from "../hooks";
 
 export function CocktailCard(): ReactElement {
-  const { name } = useCocktailsContext();
+  const { name, cocktail } = useCocktailsContext();
   return (
     <div className="card">
       {name}
+      <p>Concktail id: {cocktail?.idDrink}</p>
       {/* <img src="../assets/poster.png" alt="" /> */}
       {/* <h1>Header {name}</h1>
       <p>Header 2</p> */}
