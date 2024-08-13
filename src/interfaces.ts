6be55849e0cab916d82f0645683e5b0901bd5fb2
Mikael Fiala-Lundstrom <millom@gmail.com>
2024-08-13 13:33:23 +0200

    ICocktail: Add some fields

--- src/interfaces.ts
+++ src/interfaces.ts
@@ -1,5 +1,10 @@
 export interface ICocktail {
   idDrink: string;
+  name: string;
+  imgSrc: string;
+  category: string;
+  isAlcoholic: boolean;
+  ingredients: string[15];
 }
 
 export interface ICocktailContext {
