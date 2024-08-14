0cc3c3c9a1a6fd0d70c00fbfed36c3a4bcf4c6f1
Mikael Fiala-Lundstrom <millom@gmail.com>
2024-08-14 12:06:50 +0200

    LandingPage: Some cleanup

--- src/pages/LandingPage.tsx
+++ src/pages/LandingPage.tsx
@@ -36,8 +36,6 @@ export function LandingPage(): ReactElement {
 
   return (
     <div className="main-content">
-      <h1>LandingPage</h1>
-      <p>LandingPage 2</p>
       <CocktailCard cocktail={cocktail} />
       <button onClick={handleUpdateRandomClick}>New cocktail</button>
     </div>
