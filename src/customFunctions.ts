bf7a5939ce71902fd8fbaee246fe27988c043b8e
Mikael Fiala-Lundstrom <millom@gmail.com>
2024-08-15 14:08:53 +0200

    customFunctions: Fix mapping json -> IConcktail[]

--- src/customFunctions.ts
+++ src/customFunctions.ts
@@ -1,33 +1,147 @@
 import { ICocktail, IJSON } from "./interfaces";
 
+// const mapJsonToStringJSon: (jsonArr: IJSON[]) => IJSON[] = (jsonArr) => {
+//   return jsonArr.map((json: IJSON) => {
+//     const jsonStrObj: IJSON = {
+//       idDrink: json.idDrink,
+//       strDrink: json.strDrink,
+//       strAlcoholic: json.strAlcoholic,
+//       strTags: json.strTags,
+//       strGlass: json.strGlass,
+//       strDrinkThumb: json.strDrinkThumb,
+//       strCategory: json.strCategory,
+//       strIngredient1: json.strIngredient1 !== null ? json.strIngredient1 : "",
+//       strIngredient2: json.strIngredient2 !== null ? json.strIngredient1 : "",
+//       strIngredient3: json.strIngredient3 !== null ? json.strIngredient1 : "",
+//       strIngredient4: json.strIngredient4 !== null ? json.strIngredient1 : "",
+//       strIngredient5: json.strIngredient5 !== null ? json.strIngredient1 : "",
+//       strIngredient6: json.strIngredient6 !== null ? json.strIngredient1 : "",
+//       strIngredient7: json.strIngredient7 !== null ? json.strIngredient1 : "",
+//       strIngredient8: json.strIngredient8 !== null ? json.strIngredient1 : "",
+//       strIngredient9: json.strIngredient9 !== null ? json.strIngredient1 : "",
+//       strIngredient10: json.strIngredient10 !== null ? json.strIngredient1 : "",
+//       strIngredient11: json.strIngredient11 !== null ? json.strIngredient1 : "",
+//       strIngredient12: json.strIngredient12 !== null ? json.strIngredient1 : "",
+//       strIngredient13: json.strIngredient13 !== null ? json.strIngredient1 : "",
+//       strIngredient14: json.strIngredient14 !== null ? json.strIngredient1 : "",
+//       strIngredient15: json.strIngredient15 !== null ? json.strIngredient1 : "",
+//     };
+//     return jsonStrObj;
+//   });
+// };
+
 export const jsonToCocktails: (drinks: IJSON[]) => ICocktail[] = (
   drinks: IJSON[]
 ) => {
   return drinks.map((json: IJSON) => {
+    console.log(typeof json, json);
     const cocktail: ICocktail = {
       id: json.idDrink,
       name: json.strDrink,
+      glass: json.strGlass,
       category: json.strCategory,
       isAlcoholic: json.strAlcoholic === "Alcoholic",
       imgSrc: json.strDrinkThumb,
-      ingredients: [
-        json.strIngredient1,
-        json.strIngredient2,
-        json.strIngredient3,
-        json.strIngredient4,
-        json.strIngredient5,
-        json.strIngredient6,
-        json.strIngredient7,
-        json.strIngredient8,
-        json.strIngredient9,
-        json.strIngredient10,
-        json.strIngredient11,
-        json.strIngredient12,
-        json.strIngredient13,
-        json.strIngredient14,
-        json.strIngredient15,
-      ],
+      tags: [],
+      ingredients: [],
+      mesurements: [],
     };
+    if (json.strTags !== null) {
+      const tagArr: string[] = json.strTags.split(",");
+      for (let i = 0; i < tagArr.length; i++) {
+        cocktail.tags?.push(tagArr[i]);
+      }
+    }
+    if (json["strIngredient1"] !== null) {
+      cocktail.ingredients?.push(json["strIngredient1"]);
+    }
+    if (json["strIngredient2"] !== null) {
+      cocktail.ingredients?.push(json["strIngredient2"]);
+    }
+    if (json["strIngredient3"] !== null) {
+      cocktail.ingredients?.push(json["strIngredient3"]);
+    }
+    if (json["strIngredient4"] !== null) {
+      cocktail.ingredients?.push(json["strIngredient4"]);
+    }
+    if (json["strIngredient5"] !== null) {
+      cocktail.ingredients?.push(json["strIngredient5"]);
+    }
+    if (json["strIngredient6"] !== null) {
+      cocktail.ingredients?.push(json["strIngredient6"]);
+    }
+    if (json["strIngredient7"] !== null) {
+      cocktail.ingredients?.push(json["strIngredient7"]);
+    }
+    if (json["strIngredient8"] !== null) {
+      cocktail.ingredients?.push(json["strIngredient8"]);
+    }
+    if (json["strIngredient9"] !== null) {
+      cocktail.ingredients?.push(json["strIngredient9"]);
+    }
+    if (json["strIngredient10"] !== null) {
+      cocktail.ingredients?.push(json["strIngredient10"]);
+    }
+    if (json["strIngredient11"] !== null) {
+      cocktail.ingredients?.push(json["strIngredient11"]);
+    }
+    if (json["strIngredient12"] !== null) {
+      cocktail.ingredients?.push(json["strIngredient12"]);
+    }
+    if (json["strIngredient13"] !== null) {
+      cocktail.ingredients?.push(json["strIngredient13"]);
+    }
+    if (json["strIngredient14"] !== null) {
+      cocktail.ingredients?.push(json["strIngredient14"]);
+    }
+    if (json["strIngredient15"] !== null) {
+      cocktail.ingredients?.push(json["strIngredient15"]);
+    }
+    if (json["strMeasure1"] !== null) {
+      cocktail.mesurements?.push(json["strMeasure1"]);
+    }
+    if (json["strMeasure2"] !== null) {
+      cocktail.mesurements?.push(json["strMeasure2"]);
+    }
+    if (json["strMeasure3"] !== null) {
+      cocktail.mesurements?.push(json["strMeasure3"]);
+    }
+    if (json["strMeasure4"] !== null) {
+      cocktail.mesurements?.push(json["strMeasure4"]);
+    }
+    if (json["strMeasure5"] !== null) {
+      cocktail.mesurements?.push(json["strMeasure5"]);
+    }
+    if (json["strMeasure6"] !== null) {
+      cocktail.mesurements?.push(json["strMeasure6"]);
+    }
+    if (json["strMeasure7"] !== null) {
+      cocktail.mesurements?.push(json["strMeasure7"]);
+    }
+    if (json["strMeasure8"] !== null) {
+      cocktail.mesurements?.push(json["strMeasure8"]);
+    }
+    if (json["strMeasure9"] !== null) {
+      cocktail.mesurements?.push(json["strMeasure9"]);
+    }
+    if (json["strMeasure10"] !== null) {
+      cocktail.mesurements?.push(json["strMeasure10"]);
+    }
+    if (json["strMeasure11"] !== null) {
+      cocktail.mesurements?.push(json["strMeasure11"]);
+    }
+    if (json["strMeasure12"] !== null) {
+      cocktail.mesurements?.push(json["strMeasure12"]);
+    }
+    if (json["strMeasure13"] !== null) {
+      cocktail.mesurements?.push(json["strMeasure13"]);
+    }
+    if (json["strMeasure14"] !== null) {
+      cocktail.mesurements?.push(json["strMeasure14"]);
+    }
+    if (json["strMeasure15"] !== null) {
+      cocktail.mesurements?.push(json["strMeasure15"]);
+    }
     return cocktail;
   });
 };
--- src/interfaces.ts
+++ src/interfaces.ts
@@ -1,33 +1,53 @@
 export interface IJSON {
   idDrink: string;
   strDrink: string;
+  strTags: string;
+  strGlass: string;
   strDrinkThumb: string;
   strCategory: string;
   strAlcoholic: string;
-  strIngredient1: string;
-  strIngredient2: string;
-  strIngredient3: string;
-  strIngredient4: string;
-  strIngredient5: string;
-  strIngredient6: string;
-  strIngredient7: string;
-  strIngredient8: string;
-  strIngredient9: string;
-  strIngredient10: string;
-  strIngredient11: string;
-  strIngredient12: string;
-  strIngredient13: string;
-  strIngredient14: string;
-  strIngredient15: string;
+  strIngredient1: string | null;
+  strIngredient2: string | null;
+  strIngredient3: string | null;
+  strIngredient4: string | null;
+  strIngredient5: string | null;
+  strIngredient6: string | null;
+  strIngredient7: string | null;
+  strIngredient8: string | null;
+  strIngredient9: string | null;
+  strIngredient10: string | null;
+  strIngredient11: string | null;
+  strIngredient12: string | null;
+  strIngredient13: string | null;
+  strIngredient14: string | null;
+  strIngredient15: string | null;
+  strMeasure1: string | null;
+  strMeasure2: string | null;
+  strMeasure3: string | null;
+  strMeasure4: string | null;
+  strMeasure5: string | null;
+  strMeasure6: string | null;
+  strMeasure7: string | null;
+  strMeasure8: string | null;
+  strMeasure9: string | null;
+  strMeasure10: string | null;
+  strMeasure11: string | null;
+  strMeasure12: string | null;
+  strMeasure13: string | null;
+  strMeasure14: string | null;
+  strMeasure15: string | null;
 }
 
 export interface ICocktail {
   id: string;
   name: string;
+  tags?: string[];
+  glass: string;
   imgSrc: string;
   category: string;
   isAlcoholic: boolean;
-  ingredients: Array<string>;
+  ingredients?: Array<string>;
+  mesurements?: Array<string>;
 }
 
 export interface ICocktailContext {
