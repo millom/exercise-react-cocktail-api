6da3fb1e2c84d2337ae69442473b39ffb23ec53c
Mikael Fiala-Lundstrom <millom@gmail.com>
2024-08-15 09:32:35 +0200

    SearchPage: Remove commented out code

--- src/pages/SearchPage.tsx
+++ src/pages/SearchPage.tsx
@@ -3,7 +3,6 @@ import { ICocktail } from "../interfaces";
 import { jsonToCocktail } from "../customFunctions";
 import { useNavigate } from "react-router-dom";
 import ReactPaginate from "react-paginate";
-// import { CocktailCard } from "../components/CocktailCard";
 
 export function SearchPage(): ReactElement {
   const itemsPerPage: number = 10;
@@ -11,19 +10,9 @@ export function SearchPage(): ReactElement {
   const searchStringRef = useRef<HTMLInputElement>(null);
   const defaultCocktailList: Array<ICocktail> = [];
   const [cocktailList, setCocktailList] = useState(defaultCocktailList);
-
-  // const [page, setPage] = useState(0);
-  // const [filterData, setFilterData] = useState();
-
   const [itemOffset, setItemOffset] = useState(0);
-  // const [endOffset, setEndOffset] = useState(0);
   const [currentItems, setCurrentItems] = useState(defaultCocktailList);
   const [pageCount, setPageCount] = useState(0);
-  // const [initialPage, setInitialPage] = useState(0);
-  // const [paginateMenuCss, setPaginateMenuCss] = useState("paginate-menu");
-  // let endOffset: number = 0; // itemOffset + itemsPerPage;
-  // let currentItems: ICocktail[] = []; //items.slice(itemOffset, endOffset);
-  // let pageCount: number = // Math.ceil(items.length / itemsPerPage);
 
   const handleSearchCocktailsClick: (
     event: MouseEvent<HTMLFormElement>
@@ -31,24 +20,12 @@ export function SearchPage(): ReactElement {
     event.preventDefault();
     console.log("handleSearchCocktailsClick");
 
-    // setItemOffset(2);
-    // setInitialPage(0);
-    // setInitialPage(-1);
-
     const ul: HTMLUListElement = document.querySelector(".paginate-menu")!;
     const clickEvent = new Event("click", {
       bubbles: true,
       cancelable: true,
     });
     ul?.childNodes[1].childNodes[0].dispatchEvent(clickEvent);
-    // ul?.children[1].dispatchEvent(new Event("click"));
-    // for (let i: number = 0; i < ul?.childElementCount; i++) {
-    //   console.log("1", i, ul?.children[i]);
-    //   ul?.children[i].classList.remove("selected");
-    //   console.log("2", ul?.children[i].classList);
-    // }
-    // ul?.children[1].classList.add("selected");
-    // ul?.children[1].children[0].;
 
     const updateCocktailList = async () => {
       const response = await fetch(
@@ -59,10 +36,6 @@ export function SearchPage(): ReactElement {
       );
       const data = await response.json();
       console.log("data:", data, data.drinks);
-      // setInitialPage(0);
-      // setPaginateMenuCss(
-      //   cocktailList.length <= 10 ? "paginate-menu hidden" : "paginate-menu"
-      // );
 
       if (data?.drinks === null) {
         setCocktailList([]);
@@ -72,65 +45,23 @@ export function SearchPage(): ReactElement {
 
       const newCocktailList: ICocktail[] = jsonToCocktail(data.drinks);
 
-      // setCocktailList([]);
-      // setPageCount(0);
       setCocktailList(newCocktailList);
       setPageCount(Math.ceil(newCocktailList.length / itemsPerPage));
       console.log(pageCount);
     };
 
     updateCocktailList();
-    // .then((data) => {
-    //   console.log("ICocktail", data);
-    //   console.log("ICocktail.drinks", data.drinks);
-    //   // const cocktail: ICocktail[] = data.drinks.map((item) => {
-    //   //   idDrink: item.idDrink;
-    //   // });
-
-    //   // Working
-    //   // const cocktail: ICocktail = {
-    //   //   id: data.drinks[0].idDrink,
-    //   // };
-    //   const cocktail2: ICocktail[] = data.drinks.map((item: ICocktail) => {
-    //     console.log("map:", typeof item, item);
-    //     const cocktail: ICocktail = {
-    //       idDrink: item.idDrink,
-    //     };
-    //     return cocktail;
-    //   });
-
-    //   // });
-    //   // const cocktail: ICocktail = JSON.parse(data);
-    //   console.log("cocktail", cocktail);
-    //   console.log("cocktail2", cocktail2);
-    // });
   };
 
   useEffect(() => {
     console.log("Effect");
     const endOffset = itemOffset + itemsPerPage;
-    // setEndOffset(itemOffset + itemsPerPage);
-
-    // console.log("Effect", itemOffset, endOffset);
-    // console.log("Effect", itemOffset, endOffset);
     const newCurrentItems = cocktailList.slice(itemOffset, endOffset);
-    // setCurrentItems(newCurrentItems);
     setCurrentItems(newCurrentItems);
-    // setPageCount(Math.ceil(cocktailList.length / itemsPerPage));
-    // console.log("Effect", itemOffset, endOffset);
-    // console.log(cocktailList);
   }, [itemOffset, itemsPerPage, pageCount, cocktailList]);
-  // useEffect(() => {
-  //   setFilterData(
-  //     currentItems.filter((item, index) => {
-  //       return (index >= page * n) & (index < (page + 1) * n);
-  //     })
-  //   );
-  // }, [page]);
 
   const handlePageClick: (event: any) => void = (event: any) => {
     // event.preventDefault();
-
     const newOffset = (event.selected * itemsPerPage) % cocktailList.length;
     console.log(
       `User requested page number ${event.selected}, which is offset ${newOffset}`
@@ -151,14 +82,11 @@ export function SearchPage(): ReactElement {
           ref={searchStringRef}
           defaultValue={"margarita"}
         />
-        {/* <button onClick={() => handleSearchCocktailsClick()}>Search</button> */}
         <button type="submit">Search</button>
       </form>
       <div className="cocktail-container">
         <ul className="ul">
-          {/* {cocktailList.map((cocktail) => ( */}
           {currentItems?.map((cocktail) => (
-            // <CocktailCard cocktail={cocktail} />
             <li
               key={cocktail.id}
               className="list-item"
@@ -174,20 +102,11 @@ export function SearchPage(): ReactElement {
           breakLabel="..."
           nextLabel="next >"
           onPageChange={(event) => handlePageClick(event)}
-          // onPageChange={(event) => setPage(event.selected)}
           pageRangeDisplayed={itemsPerPage}
           pageCount={pageCount}
           previousLabel="< prev"
           renderOnZeroPageCount={null}
-          // className={paginateMenuCss}
           className={pageCount <= 1 ? "paginate-menu hidden" : "paginate-menu"}
-          // forcePage={initialPage}
-          // onPageActive={() => {
-          //   setInitialPage(0);
-          // }}
-          // initialPage={initialPage}
-          // page={initialPage}
-          // initialPage={initialPage}
         ></ReactPaginate>
       </div>
     </div>
