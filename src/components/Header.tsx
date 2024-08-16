0014cef7b6d5b446211cf00adcffb7f39d420e26
Mikael Fiala-Lundstrom <millom@gmail.com>
2024-08-16 12:32:41 +0200

    Working layout

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,11 +1,12 @@
 import { ReactElement } from "react";
-import { useCocktailsContext } from "../hooks";
+// import { useCocktailsContext } from "../hooks";
 
 export function Header(): ReactElement {
-  const { name } = useCocktailsContext();
+  // const { name } = useCocktailsContext();
   return (
     <div className="header">
-      {/* <img src="../assets/poster.png" alt="" /> */}
+      {/* <img className="header-img" src="../assets/poster.png" alt="poster" /> */}
+      <div className="header-img"></div>
       {/* <h1>Header {name}</h1>
       <p>Header 2</p> */}
     </div>
